9a873015d53b930c9543259acab822ca60f0e86c
Jack Pritchard <jackdavieswork@hotmail.co.uk>
2019-09-30 17:24:36 +0100

    Loop people in a list format

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,11 +23,16 @@ const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Using a CSV as a data source in Gatsby</h1>
-    {data.allPeopleCsv.nodes.length > 0 &&
-      data.allPeopleCsv.nodes.map(person => (
-        <Link to={`${person.FirstName}-${person.LastName}`}>
-          {person.FirstName}
-        </Link>
-      ))}
+    <p>These people were found in the CSV file.</p>
+    <ul>
+      {data.allPeopleCsv.nodes.length > 0 &&
+        data.allPeopleCsv.nodes.map(person => (
+          <li>
+            <Link to={`${person.FirstName}-${person.LastName}`}>
+              {person.FirstName}
+            </Link>
+          </li>
+        ))}
+    </ul>
   </Layout>
 )
